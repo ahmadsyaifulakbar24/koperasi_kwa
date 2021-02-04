3ee2f938171212d9b28975952f85421ac0307dcf
ipulbelcram <ipulbelcram@gmail.com>
2021-02-04 17:23:05 +0700

    cors origin

--- routes/api.php
+++ routes/api.php
@@ -29,6 +29,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+    Route::group(['prefix' => 'auth'], function () {
+        Route::post('/login', LoginController::class);
+        Route::post('/register', RegisterController::class);
+    });
 });
 
 Route::group(['middleware'  => 'auth:sanctum'], function () {
@@ -66,9 +70,4 @@ Route::group(['middleware'  => 'auth:sanctum'], function () {
     Route::group(['prefix' => 'main_setting'], function () {
         Route::post('/update', [MainSettingController::class, 'update']);
     });
-});
-
-Route::group(['prefix' => 'auth'], function () {
-    Route::post('/login', LoginController::class);
-    Route::post('/register', RegisterController::class);
 });
