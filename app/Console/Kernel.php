a364fd3245b7474ff93066e468526bec06ca5983
ipulbelcram <ipulbelcram@gmail.com>
2021-02-08 13:33:08 +0700

    coba crone job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\DB;
 
 class Kernel extends ConsoleKernel
 {
@@ -27,6 +28,15 @@ class Kernel extends ConsoleKernel
         // insert tagihan simpanan wajib setiap bulan
         // $schedule->command('insert:tagihanSimpananWajib')->monthlyOn(3, '08:00');
         // $schedule->command('insert:tagihanPinjaman')->monthlyOn(3, '08:00');
+        $schedule->call(function () {
+            DB::table('params')->insert([
+                'param_id' => null,
+                'category_param' => 'jabatan',
+                'param' => 'Sales / Marketing',
+                'order' => 4,
+                'active' => 1,
+            ]);
+        })->everyMinute();
     }
 
     /**
