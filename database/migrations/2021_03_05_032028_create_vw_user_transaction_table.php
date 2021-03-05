7c5c6aaa5273909b832c6781bdbefcafd7625d3a
ipulbelcram <ipulbelcram@gmail.com>
2021-03-05 10:30:25 +0700

    add vw_user_transaction

---
+++ database/migrations/2021_03_05_032028_create_vw_user_transaction_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVwUserTransactionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement("DROP VIEW IF EXISTS vw_user_transaction");
+        DB::statement("
+        CREATE VIEW vw_user_transaction as 
+        select 
+            a.id AS user_id, 
+            a.code AS code, 
+            b.id AS transactions_id, 
+            b.pinjaman_id AS pinjaman_id, 
+            b.type AS type_transaction, 
+            b.approved_date AS transaction_approved_date, 
+            b.created_at AS transaction_created_at, 
+            c.id AS sub_transactions_id, 
+            c.type AS type_sub_transaction, 
+            c.besaran AS besaran 
+            from 
+            (koperasi_kwa.sub_transactions c 
+            left join (koperasi_kwa.transactions b 
+            left join koperasi_kwa.users a on((a.id = b.user_id))) on((b.id = c.transaction_id)))
+        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vw_user_transaction');
+    }
+}
