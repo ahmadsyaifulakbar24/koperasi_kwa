211611656565f33162e4f0d8b83d05a88e9fbd68
syaiful <ahmadsyaifulakbar24@gmail.com>
2021-04-06 10:00:10 +0700

    repair api get pinjaman controller

--- app/Http/Controllers/API/Pinjaman/GetPinjamanController.php
+++ app/Http/Controllers/API/Pinjaman/GetPinjamanController.php
@@ -42,7 +42,7 @@ class GetPinjamanController extends Controller
     public function cek_pinjaman()
     {
         $user_id = auth()->user()->id;
-        $cek_pinjaman = Pinjaman::where('user_id', $user_id)->orWhere('status', 'pending')->orWhere('approved')->count();
+        $cek_pinjaman = Pinjaman::where([['user_id', $user_id], ['status', 'pending']])->orWhere([['user_id', $user_id], ['status', 'approved']])->count();
         if($cek_pinjaman > 0) {
             $cek = 'false';
         } else {
